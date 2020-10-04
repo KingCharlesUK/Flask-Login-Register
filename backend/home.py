52febd1d605b123e83619a71f4a3f42492ea57a2
ImPapi <47257692+ImPapi@users.noreply.github.com>
2020-10-04 23:33:03 +0100

    Update home.py

--- backend/home.py
+++ backend/home.py
@@ -1,7 +1,13 @@
 from flask import Blueprint, render_template
+from flask_login import LoginManager, login_required, current_user
+
+from models import db, Users
 
 home = Blueprint('home', __name__, template_folder='../frontend')
+login_manager = LoginManager()
+login_manager.init_app(home)
 
 @home.route('/home', methods=['GET'])
+@login_required
 def show():
     return render_template('home.html')
